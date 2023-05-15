1555cfad059ffafe7bbeb71c359f2a2565d8bb62
Divyanshu Kamble <97751164+divyanshu-kamble@users.noreply.github.com>
2023-05-15 17:48:06 +0530

    Update script.sh

--- environments/DEV/microservice-deployment/vote-application/ansible/files/script.sh
+++ environments/DEV/microservice-deployment/vote-application/ansible/files/script.sh
@@ -4,12 +4,10 @@ APPNAME=$1
 #Check for depenedincies of redis and postgres
 
 #Using key value pair - TO ITERATE OVER THE REQUIRED DEPENDENCIES AND THEN RUNNING THE CONTAINERS
-declare -a CONTAINER_NAME
-
+declare -A CONTAINER_NAME
 
 CONTAINER_NAME[redis:latest]="docker run -d -h redis -p 6379:6379 --name redis redis:latest"
 CONTAINER_NAME[postgres:15-alpine]="docker run -d -h db --name postgres -e POSTGRES_DB=db -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -p 5432:5432 postgres:15-alpine"
-CONTAINER_NAME[$APPNAME]="docker run -d -p 5000:80 --link redis --name $APPNAME divyanshuk/$APPNAME"
 
 
 #ITERATING OVER THE CONTAINER_NAME AND INSTALLING OR STARTING THE CONTAINERS
@@ -42,3 +40,28 @@ do
         # eval $DOCKER_COMMAND
     fi
 done
+
+DOCKER_COMMAND="docker run -d -p 5000:80 --link redis --name $APPNAME divyanshuk/$APPNAME"
+
+container_info=$(docker ps -a --filter "ancestor=$APPNAME" --format "{{.Names}}:{{.State}}")
+    echo "$container_info"
+    #Check if the output of the container is not null
+    if [ -n "$container_info" ]; then
+        container_name=$(echo "$container_info" | cut -d':' -f1)
+        container_status=$(echo "$container_info" | cut -d':' -f2)
+
+        if [ "$container_status" == "running" ] ; then
+            echo "Container $container_name is up and running"
+        elif [ "$container_status" == "exited" ]; then
+            echo "Container $container_name is in exited state"
+            # If the container is in exited state then start the container
+            docker start $container_name
+        else
+            echo "will install $container_name"
+            eval $DOCKER_COMMAND
+            
+        fi
+    else
+        echo "No containers found using the specified image $container_name"
+        eval $DOCKER_COMMAND
+    fi
