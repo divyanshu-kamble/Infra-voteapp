c610dda5b8d2adb5788f408db5a12a8f04e2e35d
Divyanshu Kamble <97751164+divyanshu-kamble@users.noreply.github.com>
2023-05-15 17:41:26 +0530

    Update script.sh

--- environments/DEV/microservice-deployment/vote-application/ansible/files/script.sh
+++ environments/DEV/microservice-deployment/vote-application/ansible/files/script.sh
@@ -4,12 +4,12 @@ APPNAME=$1
 #Check for depenedincies of redis and postgres
 
 #Using key value pair - TO ITERATE OVER THE REQUIRED DEPENDENCIES AND THEN RUNNING THE CONTAINERS
-declare -A CONTAINER_NAME
+declare -a CONTAINER_NAME
 
 
 CONTAINER_NAME[redis:latest]="docker run -d -h redis -p 6379:6379 --name redis redis:latest"
-CONTAINER_NAME[$APPNAME]="docker run -d -p 5000:80 --link redis --name $APPNAME divyanshuk/$APPNAME"
 CONTAINER_NAME[postgres:15-alpine]="docker run -d -h db --name postgres -e POSTGRES_DB=db -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -p 5432:5432 postgres:15-alpine"
+CONTAINER_NAME[$APPNAME]="docker run -d -p 5000:80 --link redis --name $APPNAME divyanshuk/$APPNAME"
 
 
 #ITERATING OVER THE CONTAINER_NAME AND INSTALLING OR STARTING THE CONTAINERS
